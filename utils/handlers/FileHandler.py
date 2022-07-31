e93041d4ad1c7c30faf3854da22a59bb00f7983d
Matheus Phelipe <matheusphalves@gmail.com>
2022-07-31 11:28:30 -0300

    create project

---
+++ utils/handlers/FileHandler.py
@@ -0,0 +1,6 @@
+from mimetypes import init
+
+
+class Filehandler:
+    def __init__(self) -> None:
+        pass
---
+++ utils/parsers/AbstractParser/AbstractParser.py
@@ -0,0 +1,3 @@
+class AbstractParser:
+    def __init__(self) -> None:
+        pass
